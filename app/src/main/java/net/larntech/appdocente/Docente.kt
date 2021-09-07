c3dc36f390d4f698df7f498a9ef617efcba54e05
giancarlos1230 <gianjiron98@gmail.com>
2021-09-07 13:23:31 -0600

    declaraciones de mas variables

--- app/src/main/java/net/larntech/appdocente/Docente.kt
+++ app/src/main/java/net/larntech/appdocente/Docente.kt
@@ -8,8 +8,15 @@ import java.io.Serializable
 @Entity(tableName = "tbdocentes")
 class Docente(
     val nombre: String,
-    val salario: Double,
+    val apellido: String,
+    val sexo: String,
+    val fechaNacimiento: String,
+    val ciudad: String,
     val direccion: String,
+    val padecimiento: String,
+    val celular: Double,
+    val fechaIngreso :String,
+    val salario: Double,
 
     @PrimaryKey(autoGenerate = true)
     var idDocente: Int = 0
